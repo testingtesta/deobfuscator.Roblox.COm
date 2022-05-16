afd415568ec6ec2a95dac9db01a214942cc10cef
testingtesta <104733450+testingtesta@users.noreply.github.com>
2022-05-16 02:08:00 -0400

    modjsfj

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    let text;
+    let person = prompt("Please enter a roblox obfuscator:", "");
+    if (person == null || person == "") {
+      text = "NOOOO.";
+    } else {
+        text = ""
+    }
+    document.getElementById("demo").innerHTML = (table.concat(text));
+  }
